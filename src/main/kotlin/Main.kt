d3e8b2f0f7f3de84357843de3f15837e78841846
blueishot <fabricioteixeirahungria@gmail.com>
2022-02-24 21:40:44 -0300

    src

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("test")
+    var product = "chocolate"
+    val business = "Losangulo"
+    var work:String = "emprego"
+    product = "biscoito"
+    println(product::class)
+    println(business)
+    val pergunta = 10
+    val resposta = pergunta.toDouble()
+    println(resposta)
+    // isso é um comentário
+    // git test
+
 }
