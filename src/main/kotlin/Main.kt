be7d2cc3df513ee248a8656dd04c6ac5b3440f2e
blueishot <fabricioteixeirahungria@gmail.com>
2022-02-27 23:34:15 -0300

    Operadores, char e formatação

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 fun main() {
+    // Aula 1
     var product = "chocolate"
     val business = "Losangulo"
     var work:String = "emprego"
@@ -11,4 +12,56 @@ fun main() {
     // isso é um comentário
     // git test
 
+    //Aula 2
+
+    val numero:Long = 1_000_000
+    println(numero)
+
+    //Operadores (+ - * /)
+
+    var idade = 10
+    idade = idade + 12
+    println(idade)
+
+    val booleana = false
+    println(booleana)
+    println(!booleana)
+
+    //char
+
+    val userName = "Caio"
+    val userNameFirstLetter = 'C'
+    val pularLinha = '\n'
+    val identar = '\t'
+    println(userNameFirstLetter)
+    println(userName + pularLinha + userNameFirstLetter)
+    println(userName + identar + userNameFirstLetter)
+
+    val tamanhoDoUserName = userName.length
+    println(tamanhoDoUserName)
+
+    val nomesMaiusculos = userName.uppercase()
+    println(nomesMaiusculos)
+
+    val nomesMinus = userName.lowercase()
+    println(nomesMinus)
+
+    val mensagem = """
+       Olá Usuário,
+       Seu produto chegou!
+    """.trimIndent()
+
+    val mensagem2 = """
+        Olá Usuário,
+        Seu produto chegou!
+    """.replaceIndent(newIndent = ";")
+
+    println(mensagem)
+    println(mensagem2)
+
+    val usuario = "Diego"
+    val usuarioIdade = 42
+    println("Olá " + usuario + ", Sua idade é " + usuarioIdade)
+    println("Olá $usuario, sua idade é $usuarioIdade")
+    println("Sua idade ano que vem é de ${usuarioIdade + 1}")
 }
